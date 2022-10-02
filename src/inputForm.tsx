03f342e7d5b7769aa43b03bfd3e0b2c5e306c574
VigneshSaravanan <76770922+VigneshSaravanan26@users.noreply.github.com>
2022-10-02 22:31:39 +0530

    css and input form tsx file added

---
+++ src/inputForm.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface NewNoteInputProps {
+  addNote(note: string): void;
+}
+
+const NewNoteInput: React.FC<NewNoteInputProps> = ({ addNote }) => {
+  const [note, setNote] = React.useState("");
+
+  const updateNote = (e: any) => {
+    setNote(e.target.value);
+  };
+
+  const onAddNoteClick = () => {
+    addNote(note);
+    setNote("");
+  };
+  return (
+    <div>
+      <input
+        onChange={updateNote}
+        value={note}
+        type="text"
+        name="name"
+        placeholder="Note"
+      />
+      <button onClick={onAddNoteClick}>Add Note</button>
+    </div>
+  );
+};
+
+export default NewNoteInput;
